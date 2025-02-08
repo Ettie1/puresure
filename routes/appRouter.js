a8a7c511269632ae7ea046371333a286c10f8a6a
Ettie1 <efverwant@gmail.com>
2025-02-08 21:14:16 +0200

    AddAddPolicyAndDeletePol

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const con = require('../dat/mysqlconn')
+const { error } = require('jquery')
 
 router.get('/', (req, res)=>{
     res.render('app/phmenu')
@@ -13,8 +14,43 @@ router.get('/policyholders', (req, res)=>{
         const strDat = JSON.stringify(result);
         const jDat = JSON.parse(strDat)
         res.render('app/policyholders', {appDat: jDat})
-    })
+    }) 
+})
+
+router.get('/addpolicyholder', (req, res)=>{
+    res.render('app/addPolicyholder')
+})
+
+router.post('/addpolicyholder', (req, res)=>{
+    let title = req.body.title != null ? req.body.title : ''
+    let firstname = req.body.firstname != null ? req.body.firstname : ''
+    let lastname = req.body.lastname != null ? req.body.lastname : ''
+    let age = req.body.age != null ? req.body.age : ''
+    con.query(`INSERT INTO Policyholder(Title, Firstname, Lastname, Age)values('${title}','${firstname}','${lastname}','${age}')`,
+        (error, result, fields)=>{
+            if(error) throw error
+
+            res.redirect('/app')
+        })
     
 })
+
+
+
+router.get('/deletepolicyholder', (req, res)=>{
+    res.render('app/deletePolicyholder')
+})
+
+// router.post('/deletepolicyholder')
+
+router.get('/getph/:lastname', (req, res)=>{
+    con.query(`SELECT * FROM Policyholder WHERE Lastname = '${req.params.lastname}'`,
+        (error, result, fields)=>{
+            if(error) throw error
+
+            return JSON.stringify(result)
+        })
+})
+
 
 module.exports = router
